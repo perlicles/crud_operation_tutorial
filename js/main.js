66088056e54ff68348b218f99811f763a3dba427
perlicles <perlicles@gmail.com>
2020-04-03 07:40:26 -0300

    Button Create created

---
+++ js/main.js
@@ -0,0 +1,33 @@
+import productdb, {
+    bulkcreate
+} from './Module.js';
+
+let db = productdb("Productdb",{
+    produtcs: '++id, name, seller, price'
+});
+
+// input tags
+const userid = document.getElementById("userid");
+const proname = document.getElementById("proname");
+const seller = document.getElementById("seller");
+const price = document.getElementById("price");
+
+// buttons
+const btncreate = document.getElementById("btn-create");
+const btnread = document.getElementById("btn-read");
+const btnupdate = document.getElementById("btn-update");
+const btndelete = document.getElementById("btn-delete");
+
+// insert value using create button
+btncreate.onclick = (event) => {
+    let flag = bulkcreate(db.products, {
+        name : proname.value,
+        seller : seller.value,
+        price : price.value
+    });
+    console.log(flag);
+}
+
+btnread.onclick = (event) => {
+   
+}
